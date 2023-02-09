ecd156a07377ea15f8b1cb336ab9639a1af574a2
unknown <anacristina.ayala90@gmail.com>
2023-02-09 15:48:22 +0100

    Ejercicio8 resuelto

--- Ejercicios_Libro/src/practica/Ejercicios.java
+++ Ejercicios_Libro/src/practica/Ejercicios.java
@@ -4,22 +4,29 @@ public class Ejercicios {
 	
 	public static void Ejercicio8() {
 		
-		int NumeroRandom = (int)(Math.random()*52); //52 letras de la A-Z,a-z.	
-		char Letra;
+		int i=0;
+		while (i<10){
+			
+	
 		
-		if (NumeroRandom < 26) { //si es menor que 26 va a ser MAYUS 
-			Letra = (char)(NumeroRandom + 65); //65 es la A en ASCII 
-		} else {//si es mayor que 26 va a ser minus
-			Letra = (char)(NumeroRandom -26 +97); //(+71), 97 es la a en ASCII 
+			int NumeroRandom = (int)(Math.random()*52); //52 letras de la A-Z,a-z.	
+			char Letra;
+			
+			if (NumeroRandom < 26) { //si es menor que 26 va a ser MAYUS 
+				Letra = (char)(NumeroRandom + 65); //65 es la A en ASCII 
+			} else {//si es mayor que 26 va a ser minus
+				Letra = (char)(NumeroRandom -26 +97); //(+71), 97 es la a en ASCII 
+			}
+			
+			if(Letra=='A' || Letra =='E'|| Letra == 'I' || Letra == 'O' || Letra == 'U'|| 
+					Letra=='a' || Letra =='e'|| Letra == 'i' || Letra == 'o' || Letra == 'u'){
+				System.out.println("La letra '" + Letra + "' es vocal");
+			}else {
+				System.out.println("La letra '" + Letra + "' es consonante");
+			}
+			
+			i++;
 		}
-		
-		if(Letra=='A' || Letra =='E'|| Letra == 'I' || Letra == 'O' || Letra == 'U'|| 
-				Letra=='a' || Letra =='e'|| Letra == 'i' || Letra == 'o' || Letra == 'u'){
-			System.out.println("La letra '" + Letra + "' es vocal");
-		}else {
-			System.out.println("La letra '" + Letra + "' es consonante");
-		}
-		
 	}
 	
 	public static void main(String []args) {
