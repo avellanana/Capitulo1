0f1f2d6d26da631510ec9fdfea276f6103224f2e
unknown <anacristina.ayala90@gmail.com>
2023-02-24 21:37:24 +0100

    Ej Exception y Restaurante

---
+++ EjercicioMaven/src/main/java/ejercicioPrueba/Persona.java
@@ -0,0 +1,15 @@
+package ejercicioPrueba;
+
+public class Persona {
+	private string nombre;
+	private boolean sexo;
+	private int edad;
+	private string dni;
+	private int peso;
+	private int altura;		
+	}
+
+
+public static void main(String[] args) {
+
+}
