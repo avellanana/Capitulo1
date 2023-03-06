a8510c1d7f07cc8e97332d9498a193492a8d6bfe
unknown <anacristina.ayala90@gmail.com>
2023-03-06 20:21:40 +0100

    nuevos ejercicios

---
+++ Ejercicios_Libro/GestionExcepciones/src/test/java/ejercicio1/TestUnit.java
@@ -0,0 +1,29 @@
+package ejercicio1;
+
+import org.junit.jupiter.api.Test;
+
+import proyectoPrueba.ExcepcionBlanco;
+import proyectoPrueba.ExcepcionNumero;
+import proyectoPrueba.ExcepcionSalida;
+import proyectoPrueba.ExcepcionVocal;
+
+public class TestUnit {
+
+	LeerPorTeclado leer1 = new LeerPorTeclado();
+			
+	@Test
+	void testSingleSuccessTest() {
+		
+		try{
+			leer1.devolverValor();
+		} catch (ExcepcionVocal e) {
+			System.out.println(e.getMessage());
+		} catch (ExcepcionBlanco e) {
+			System.out.println(e.getMessage());
+		} catch (ExcepcionNumero e) {
+			System.out.println(e.getMessage());
+		} catch (ExcepcionSalida e) {
+			System.out.println(e.getMessage());
+	}
+}
+}
